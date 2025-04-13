9ea936cafa1606aaab7988ae8101adaff476066f
luisdramirez <luis.ramirez165@gmail.com>
2025-04-12 17:51:49 -0700

    Update estimatePSF.m

--- functions/estimatePSF.m
+++ functions/estimatePSF.m
@@ -4,6 +4,9 @@
 %   Inputs:
 %       measured_BOLD - voxel time series [voxels x time]
 %       I - input time series [1 x time]
+%       HIRF - hemodynamic impulse response function [1 x time]
+%       p - structure of parameters (see example_pipeline.m)
+%       toggles - structure of toggles (see example_pipeline.m)
 %
 %   Outputs:
 %       pSFT - struct containing: 
@@ -15,56 +18,17 @@
 %       -   estimated SSE values [voxels x 1]
 %       -   fmincon exit flags [voxels x 1]
 
-function [pSFT, p, toggles] = estimatePSF(measured_BOLD, I, HIRF)
+function [pSFT, p, toggles] = estimatePSF(measured_BOLD, I, HIRF, p, toggles)
 
     tic;
-    
-    addpath('/functions');
-    addpath('/script_modules');
-    
-    %% Toggles
-    
-    toggles.parallelization = 1;
-    
-    toggles.coarse_grid_search = 1;
-    toggles.fine_grid_search = 1;
-    
-    toggles.make_voxel_pSFT_plots = 0;  % Plot individual voxel fits
-    
-    %% Parallelization setup for parfor loop
-    
-    num_cores = 8;
-    num_chunks = num_cores-1;
-    
-    if toggles.parallelization
-        maxNumCompThreads(num_cores);
-        if num_cores > 1
-            parpool('local', num_chunks)
-        end
-        toggles.make_voxel_pSFT_plots = 0; % note: figures can't be drawn during parfor loops, so this will force plot generation off.
+
+    if toggles.disp_on
+        disp('Initializing estimatePSF ...');
     end
-    
-    %% Spatial frequency parameters
-    % These are hardcoded SF parameters used for generating pSFT curves.
-    % Ideally, the range should match the range of SFs presented.
-    
-    p.sf_min = 0.1;
-    p.sf_max = 12;
-    
-    p.sf_count = 100;
-    p.sfs = 10.^linspace(log10(p.sf_min), log10(p.sf_max), p.sf_count);
-    
-    %% Initalize pSFT model parameters
-    % [mu, sigma, beta, beta_0]
-    
-    p.init_params = [1 1 1 0]; 
-    
-    p.pSFT_bounds(1,:) = [6, 4, 25, 10];
-    p.pSFT_bounds(2,:) = [0.009, 0.1, -25, -10]; 
-  
+      
     %% Initialize 'chunks' structure array 
         
-    chunk_size = cell(1, num_vox_chunks);    
+    chunk_size = cell(1, p.num_chunks);    
 
     chunks = struct('vox_indices', chunk_size, ...
         'param_est', chunk_size, ...    
@@ -80,27 +44,39 @@ function [pSFT, p, toggles] = estimatePSF(measured_BOLD, I, HIRF)
 
     %% Chunk time series data
 
-    [measured_BOLD_chunks, vox_chunk_indices] = chunkTimeSeries(num_chunks, measured_BOLD);
+    if toggles.disp_on
+        disp('Chunking time series data...');
+    end
 
-    for chunk = 1:num_chunks
+    [measured_BOLD_chunks, vox_chunk_indices] = chunkTimeSeries(p.num_chunks, measured_BOLD);
+
+    for chunk = 1:p.num_chunks
         chunks(chunk).measured_BOLD = measured_BOLD_chunks{chunk};
         chunks(chunk).vox_indices = vox_chunk_indices(chunk,:);
     end
     
     %% Loop through chunks of voxels
     
+    if toggles.disp_on
+        disp('Fitting voxels...');
+    end
+    
     if toggles.parallelization
-        parfor chunk = 1:num_chunks
-            chunks(chunk) = fitVoxels(chunks(chunk).measured_BOLD, I, HIRF, p, toggles);
+        parfor chunk = 1:p.num_chunks
+            chunks(chunk) = fitVoxels(chunks(chunk), I, HIRF, p, toggles);
         end
         delete(gcp);
     else
-        for chunk = 1:num_chunks
-            chunks(chunk) = fitVoxels(chunks(chunk).measured_BOLD, I, HIRF, p, toggles);
+        for chunk = 1:p.num_chunks
+            chunks(chunk) = fitVoxels(chunks(chunk), I, HIRF, p, toggles);
         end
     end
     
     %% Resplice chunks
+
+    if toggles.disp_on
+        disp('Resplicing chunks...');
+    end
 
     field_names = fieldnames(chunks);
 
@@ -110,6 +86,10 @@ function [pSFT, p, toggles] = estimatePSF(measured_BOLD, I, HIRF)
     
     %% Print elapsed time
 
-    disp(['Elapsed time: ~' num2str(round(toc/60,1)) ' minute(s).']);
+    p.elapsed_time = round(toc/60,1);
+    if toggles.disp_on
+        disp(['Elapsed time: ~' num2str(p.elapsed_time) ' minute(s).']);
+        disp(' ');
+    end
 
 end
