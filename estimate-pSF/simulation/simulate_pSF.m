032df37f26e11bbab3c816ce3908a773cbdee062
luisdramirez <luis.ramirez165@gmail.com>
2025-05-06 15:50:24 -0700

    added license text

--- estimate-pSF/simulation/simulate_pSF.m
+++ estimate-pSF/simulation/simulate_pSF.m
@@ -130,3 +130,22 @@ xlim([sf_min sf_max])
 ylim([0 1])
 box off;
 legend({'Baseline pSFT', [num2str(sigma_inc) ' \times \sigma'], ['\sigma / ' num2str(sigma_dec)]})
+
+%% 
+
+           % The Population Spatial Frequency Toolbox
+% Copyright (C) 2025 Luis D. Ramirez, Feiyi Wang, Emily Wiecek, Louis N. Vinke, and Sam Ling
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+% Contact luisdramirez95@gmail.com for any questions or comments.
--- measure-pSF/functions/presentStimuli.m
+++ measure-pSF/functions/presentStimuli.m
@@ -147,3 +147,22 @@ function run_info = presentStimuli(p, w, t, stimuli, frames)
     run_info.behav_data = behav_data;
 
 end
+
+%% 
+
+           % The Population Spatial Frequency Toolbox
+% Copyright (C) 2025 Luis D. Ramirez, Feiyi Wang, Emily Wiecek, Louis N. Vinke, and Sam Ling
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+% Contact luisdramirez95@gmail.com for any questions or comments.
