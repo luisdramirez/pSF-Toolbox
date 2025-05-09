4b28255a42b1d20beba8d3bee9ccd8dbba9abde7
luisdramirez <luis.ramirez165@gmail.com>
2025-05-09 12:53:29 -0700

    Update example_pipeline.m

--- estimate-pSFT/example_pipeline.m
+++ estimate-pSFT/example_pipeline.m
@@ -31,7 +31,7 @@ toggles.coarse_grid_search = true;
 toggles.fine_grid_search = true;
 toggles.disp_on = true; % Display progress
 
-save_pSF = true;
+save_pSFT = true;
 make_voxel_plots = true;
 save_voxel_plots = true;
 
@@ -41,7 +41,7 @@ checkRequiredToolboxes(toggles);
 
 %% Parallelization setup for parfor loop
 
-p.num_cores = 8;
+p.num_cores = 4;
 p.num_chunks = p.num_cores-1;
 
 if toggles.parallelization
@@ -52,28 +52,23 @@ if toggles.parallelization
 end
 
 %% Spatial frequency parameters
-% These are hardcoded SF parameters used for generating pSFT curves.
+% Hardcoded SF parameters used for generating pSFT curves.
 % Ideally, the range should match the range of SFs presented.
 
-p.sf_min = 0.1;
+p.sf_min = 0.5;
 p.sf_max = 12;
 
 p.sf_count = 100;
 p.sfs = 10.^linspace(log10(p.sf_min), log10(p.sf_max), p.sf_count);
 
 %% Initalize pSFT model parameters
-% [mu, sigma, beta, beta_0]
 
-p.initial_params = [1 1 1 0]; 
+p.initial_params = [1 1 1 0]; % [mu, sigma, beta, beta_0]
 
-p.pSFT_bounds(1,:) = [6, 4, 25, 10];
-p.pSFT_bounds(2,:) = [0.009, 0.1, -25, -10]; 
+p.pSFT_bounds(1,:) = [6, 4, 25, 10]; % upper bounds
+p.pSFT_bounds(2,:) = [0.009, 0.1, -25, -10]; % lower bounds
 
-p.fmincon_options = optimset('MaxFunEvals', 100000, 'MaxIter', 10000, 'display','off');
-
-%% Prepare data
-
-% prepareData();
+p.fmincon_options = optimset('MaxFunEvals', 100000, 'MaxIter', 10000, 'display','off'); 
 
 %% Load data
 
@@ -83,6 +78,8 @@ num_subjs = length(sample_data);
 num_ROIs = length(sample_data(1).measured_BOLD);
 
 roi_names = {'V1', 'V2', 'V3'};
+
+%% Define hemodynamic response function (HRF)
 
 HRF = defineHRF();
 
@@ -102,31 +99,30 @@ all_pSFT = struct('vox_indices', struct_size, ...
     'exitflag', struct_size, ...
     'measured_BOLD', struct_size);
 
-%% Estimate pSFT
+%% Estimate pSFT for each subject and ROI
 
 total_elapsed_time = 0;
 
 for subj = 1:num_subjs
 
-    if toggles.disp_on, disp(['+++ S ' num2str(subj) ' +++']); end
+    if toggles.disp_on, disp(['+++ S' num2str(subj) ' +++']); end
 
     I = sample_data(subj).I;
     
     for roi = 1:num_ROIs
 
-        if toggles.disp_on, disp(['-- ROI ' roi_names{roi} ' --']); end
+        if toggles.disp_on, disp(['- ' roi_names{roi} ' -']); end
 
         tic;    
 
         measured_BOLD = sample_data(subj).measured_BOLD{roi};
         
+        % Enter estimatePSFT
         pSFT = estimatePSFT(I, measured_BOLD, HRF, p, toggles);
         all_pSFT(subj,roi) = pSFT;
 
         elapsed_time = round(toc/60,1);
-
         if toggles.disp_on, disp(['Elapsed time: ~' num2str(elapsed_time) ' minute(s).']); disp(' '); end
-
         total_elapsed_time = total_elapsed_time + elapsed_time;
 
     end
@@ -169,7 +165,7 @@ if make_voxel_plots
                 % Format figure
                 xlabel('log[SF] (cpd)'); ylabel('R');
                 xlim([p.sf_min p.sf_max]); ylim([0 1]);
-                xticks([p.sf_min 0.5 1 5 p.sf_max]); xticklabels([p.sf_min 0.5 1 5 p.sf_max]); 
+                xticks([p.sf_min 1 5 p.sf_max]); xticklabels([p.sf_min 1 5 p.sf_max]); 
                 set(gca,'TickDir','out'); box off;
                 title(['\mu = ' num2str(round(all_pSFT(subj,roi).param_est(1,vox),2)) ' | \sigma = ' num2str(round(all_pSFT(subj,roi).param_est(2,vox),2))])
                 
