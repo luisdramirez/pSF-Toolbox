96e0306a27a4771580223453d8f99c52ab4df35a
luisdramirez <luis.ramirez165@gmail.com>
2025-05-09 12:53:53 -0700

    Update checkPTB.m

--- measure-pSFT/functions/checkPTB.m
+++ measure-pSFT/functions/checkPTB.m
@@ -1,10 +1,16 @@
+% checkPTB - Check if Psychtoolbox is installed
+%
+% Syntax
+%   checkPTB()
+%
+
 function checkPTB()
     if ~exist('PsychtoolboxVersion', 'file')
-        error(['Psychtoolbox-3 is not installed or not on the MATLAB path.\n' ...
+        error(['Psychtoolbox is not installed or not on the MATLAB path.\n' ...
                'Please install it from http://psychtoolbox.org/download.']);
     else
         v = PsychtoolboxVersion;
-        fprintf('Psychtoolbox-3 is installed. Version: %s\n', v);
+        fprintf('Psychtoolbox is installed. Version: %s\n', v);
     end
 end
 
